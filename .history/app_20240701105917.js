4d4bce70adc5cff9e3e4072d9c9520a616969c8a
Muhammad Azhari <muhammadazhari24102001@gmail.com>
2024-07-01 11:22:13 +0700

    first commit

---
+++ .history/app_20240701105917.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+
+const app = express();
+
+
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to semina',
+    });
+});
+
+module.exports = app;
---
+++ .history/app_20240701105952.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+
+const app = express();
+
+
+//router
+const categoriesRouter = require('./app')
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to semina',
+    });
+});
+
+module.exports = app;
